f93e582f8bcf1afcdc72758beece4952da415d34
Joel <joel.hakkinen@aalto.fi>
2022-09-21 20:09:21 +0300

    3.6

--- index.js
+++ index.js
@@ -4,44 +4,78 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-	{
-		id: 1,
-		name: "Arto Hellas",
-		number: "040-123456"
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523"
-	},
-	{
-		id: 3,
-		name: "Dan Abramov",
-		number: "12-43-234345"
-	},
-	{
-		id: 4,
-		name: "Mary Poppendick",
-		number: "39-23-6423122"
-	}
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456"
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523"
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345"
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122"
+  }
 ]
 
-app.get('/api/persons', (req, res) => {
-  res.send(persons)
+app.get('/api/persons', (request, response) => {
+  response.send(persons)
 })
 
-app.get('/info', (req, res) => {
-	const date = new Date()
-	res.send(`
-		<p>Phonebook has ${persons.length} people</p>
-		<p>${date}</p>
-	`)
+app.get('/info', (request, response) => {
+  const date = new Date()
+  response.send(`
+    <p>Phonebook has ${persons.length} people</p>
+    <p>${date}</p>
+  `)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-	const id = request.params.id
-	const person = persons.find(p => p.id == id)
-	response.json(person)
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id == id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons/', (request, response) => {
+  const person = request.body
+  if (!person.name || !person.number) {
+    return response.status(400).json({
+      error: 'name or number missing'
+    })
+  }
+  if (persons.map(p => p.name).includes(person.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(p => p.id)) + 1
+    : 0
+  const newPerson = {
+    id: maxId,
+    name: person.name,
+    number: person.number
+  }
+  persons = persons.concat(newPerson)
+  response.status(204).end()
 })
 
 const PORT = 3001
