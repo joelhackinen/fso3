e324b0bd29488b3ff508f0d5860be87dc44a16c2
Joel <joel.hakkinen@aalto.fi>
2022-09-26 14:56:20 +0300

    3.19

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons/', (request, response) => {
+app.post('/api/persons/', (request, response, next) => {
   const content = request.body
   if (!content.name || !content.number) {
     return response.status(400).json({
@@ -76,9 +76,11 @@ app.post('/api/persons/', (request, response) => {
     name: content.name,
     number: content.number
   })
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -92,6 +94,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ message: error.message})
   }
 
   next(error)
